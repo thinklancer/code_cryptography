e66adf1303935d153225bd6dd091a9cd88731403
thinklancer <thinklancer@gmail.com>
2013-02-13 17:17:12 +0100

    near end of hw4

--- hw4.py
+++ hw4.py
@@ -17,30 +17,57 @@ Now to business. Suppose an attacker wishes to steal secret information from our
 '''
 
 import urllib2
-import sys
 
 TARGET = 'http://crypto-class.appspot.com/po?er='
-ciphertext = 'f20bdba6ff29eed7b046d1df9fb7000058b1ffb4210a580f748b4ac714c001bd4a61044426fb515dad3f21f18aa577c0bdf302936266926ff37dbf7035d5eeb4'
+ciphertext = 'f20bdba6ff29eed7b046d1df9fb7000058b1ffb4210a580f748b4ac714c001bd4a61044426fb515dad3f21f18aa577c0bdf302936266926ff37dbf7035d5eeb4'.decode('hex')
+FAST_CHARLIST = ' etaonisrhldcupfmwybgvkqxjzETAONISRHLDCUPFMWYBGVKQXJZ,.!'
 #--------------------------------------------------------------
 # padding oracle
 #--------------------------------------------------------------
-class PaddingOracle(object):
-    def query(self, q):
-        target = TARGET + urllib2.quote(q)    # Create query URL
-        req = urllib2.Request(target)         # Send HTTP request to server
-        try:
-            f = urllib2.urlopen(req)          # Wait for response
-        except urllib2.HTTPError, e:
-            print "We got: %d" % e.code       # Print response code
-            if e.code == 404:
-                return True # good padding
-            return False # bad padding
+def query(q):
+    target = TARGET + urllib2.quote(q)    # Create query URL
+    req = urllib2.Request(target)         # Send HTTP request to server
+    try:
+        f = urllib2.urlopen(req)          # Wait for response
+    except urllib2.HTTPError, e:
+        #print "We got: %d" % e.code       # Print response code
+        if e.code == 404:
+            return True # good padding
+        return False # bad padding
+
+def strxor(a, b):     # xor two strings of different lengths
+    if len(a) > len(b):
+        return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a[:len(b)], b)])
+    else:
+        return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
+
+def decryptblock(prevblock,currentblock):
+    pt0 = ['?' for i in range(16)]
+    for i in range(15,-1,-1):
+        pad_len=16-i
+        for guess in FAST_CHARLIST:
+            fullguess = pt0
+            fullguess[i] = guess
+            #print fullguess
+            new_prev_block = strxor(strxor(prevblock,fullguess),chr(pad_len)*16)
+            new_ct = new_prev_block+currentblock
+            if query(new_ct.encode('hex')):
+                print 'found it: %s' % guess
+                pt0[i] = guess
+                break
+    return pt0;
 
 def decrypt():
-    plaintext = ''
-    po = PaddingOracle()
-    po.query()
-    return plaintext
+    blocks = []
+    plaintext = []
+    for i in range(16,len(ciphertext),16):
+        blocks.append(ciphertext[i:i+16])
+    n = len(blocks)
+    for i in range(n-1):
+        s=decryptblock(blocks[i],blocks[i+1])
+        print i,blocks[i],s
+        plaintext.append(s)
+    print plaintext
 
 if __name__ == "__main__":
     print decrypt()
