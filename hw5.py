e39e8d7d66be69d6bca58f2746eac1849e9efd14
thinklancer <thinklancer@gmail.com>
2013-02-15 17:24:28 +0100

    improve the efficiency and correct a bug

--- hw5.py
+++ hw5.py
@@ -26,39 +26,42 @@ Let B=2^20. Since x is less than B^2 we can write the unknown x base B as x=x0B+
 import pickle
 from numbthy import *
 
-p=13407807929942597099574024998205846127479365820592393377723561443721764030073546976801874298166903427690031858186486050853753882811946569946433649006084171
+p= 13407807929942597099574024998205846127479365820592393377723561443721764030073546976801874298166903427690031858186486050853753882811946569946433649006084171
 
 g= 11717829880366207009516117596335367088558084999998952205599979459063929499736583746670572176471460312928594829675428279466566527115212748467589894601965568
 
-h=3239475104050450443565264378728065788649097520952449527834792452971981976143292558073856937958553180532878928001494706097394108577585732452307673444020333
-
+h= 3239475104050450443565264378728065788649097520952449527834792452971981976143292558073856937958553180532878928001494706097394108577585732452307673444020333
+   
 if __name__ == "__main__":
-    
     print len(str(p)),len(str(g)),len(str(h))
     print "calculate 2^20 array"
-    lhs = [powmod(2,i,p) for i in range(2**20)]
-    with open("temp.red","w") as f:
+    lhs = [powmod(g,i,p) for i in range(2**20)]
+    with open("temp1.red","w") as f:
         pickle.dump(lhs,f)
     f.close()
     print "calculate 1/2^20 array"
     lhs = [xgcd(i,p)[1] for i in lhs]
-    with open("temp.red","w") as f:
+    with open("temp2.red","w") as f:
         pickle.dump(lhs,f)
     f.close()
     print "calculate LHS"
     lhs = [h*i%p for i in lhs]
-    with open('temp.red','w') as f:
+    #with open('temp3.red','r') as f:
+    with open('temp3.red','w') as f:
         pickle.dump(lhs,f)
         #lhs=pickle.load(f)
     f.close()
+    # build hash table
+    dlhs = dict()
+    for id, val in enumerate(lhs):
+        dlhs[val]=id
+    
     gb = powmod(g,2**20,p)
     print "start searching ..."
+    rhs = [powmod(gb,i,p) for i in range(2**20)]
     for i in range(2**20):
-        if i % 2**12 == 0:
-            print "  finish ",i/2**12,"/256 steps"
-        rhs = powmod(gb,i,p)
-        if rhs in lhs:
-            x1=lhs.index(rhs)
+        if rhs[i] in dlhs:
+            x1=dlhs[rhs[i]]
             x0=i
             print x0,x1,x0*2**20+x1
             break
